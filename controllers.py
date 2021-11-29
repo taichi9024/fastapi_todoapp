41685485c3a27f6408b10d33f27d566e74d21b4e
noguchi <noguchi@pc.local>
2021-11-29 21:37:21 +0900

    ログイン機能追加

--- controllers.py
+++ controllers.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException 
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import security
 from starlette.templating import Jinja2Templates
 from starlette.requests import Request
- 
+from starlette.status import HTTP_401_UNAUTHORIZED
+import db
+from models import User, Task 
+import hashlib 
+import re
+
+pattern = re.compile(r"\w{4,20}")
+pattern_pw = re.compile(r"\w{6,20}")
+pattern_mail = re.compile(r'^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$')
+
+
 app = FastAPI(
     title='FastAPIでつくるtoDoアプリケーション',
     description='FastAPIチュートリアル：FastAPI(とstarlette)でシンプルなtoDoアプリを作りましょう．',
     version='0.9 beta'
 )
+
+security = HTTPBasic()
 
 templates = Jinja2Templates(directory="templates")
 jinja_env = templates.env
@@ -16,6 +30,72 @@ def index(request: Request):
     return templates.TemplateResponse("index.html",
     {"request": request})
 
-def admin(request: Request):
-    return templates.TemplateResponse("admin.html",
-    {"request": request,"username": "admin"})
+def admin(request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+    # Basic認証で受け取った情報
+    username = credentials.username
+    password = hashlib.md5(credentials.password.encode()).hexdigest()
+ 
+    # データベースからユーザ名が一致するデータを取得
+    user = db.session.query(User).filter(User.username == username).first()
+    task = db.session.query(Task).filter(Task.user_id == user.id).all() if user is not None else []
+    db.session.close()
+ 
+    # 該当ユーザがいない場合
+    if user is None or user.password != password:
+        error = 'ユーザ名かパスワードが間違っています'
+        raise HTTPException(
+            status_code=HTTP_401_UNAUTHORIZED,
+            detail=error,
+            headers={"WWW-Authenticate": "Basic"},
+        )
+ 
+    # 特に問題がなければ管理者ページへ
+    return templates.TemplateResponse('admin.html',
+                                      {'request': request,
+                                       'user': user,
+                                       'task': task
+                                       })
+    
+async def register(request: Request):
+    if request.method == "GET":
+        return templates.TemplateResponse("register.html",
+        {"request": request,
+        "username": "",
+        "error": []
+        })
+    if request.method == "POST":
+        # POSTデータ
+        data = await request.form()
+        username = data.get('username')
+        password = data.get('password')
+        password_tmp = data.get('password_tmp')
+        mail = data.get('mail')
+
+        error = []
+
+        tmp_user = db.session.query(User).filter(User.username == username).first()
+
+        if tmp_user is not None:
+            error.append("同じユーザー名のユーザが存在します")
+        if password != password_tmp:
+            error.append('入力したパスワードが一致しません。')
+        if pattern.match(username) is None:
+            error.append('ユーザ名は4~20文字の半角英数字にしてください。')
+        if pattern_pw.match(password) is None:
+            error.append('パスワードは6~20文字の半角英数字にしてください。')
+        if pattern_mail.match(mail) is None:
+            error.append('正しくメールアドレスを入力してください。')
+
+        if error:
+            return templates.TemplateResponse("register.html",
+                                              {"request": request,
+                                              "username": username,
+                                              "error": error})
+        user = User(username, password, mail)
+        db.session.add(user)
+        db.session.commit()
+        db.session.close()
+
+        return templates.TemplateResponse("complete.html",
+                                          {"request": request,
+                                          "username": username})
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from controllers import *
   
 # FastAPIのルーティング用関数
+app.add_api_route('/admin', admin)
 app.add_api_route('/', index)
-app.add_api_route("/admin", admin)
+app.add_api_route("/register", register, methods=["GET", "POST"])
